4f3d0b5ffd65ef5b775dbfd8ad3df21d42c3e0cd
asmaa <hossamasmaa07@gmail.com>
2024-11-08 23:08:16 +0200

    correct2

--- js/cardproduct.js
+++ js/cardproduct.js
@@ -9,7 +9,7 @@ if(productlocal){
 function drowitems(){
     let y=productlocal.map((item)=>{
         return`
-        <div class="col-lg-3 col-md-4 col-sm-2 mt-3 d-flex" id="parent-${item.id}">
+        <div class="col-lg-3 col-md-4 col-sm-6 mt-3 d-flex" id="parent-${item.id}">
         <div class="card p-3 shadow-sm " >
             <img class="card-img-top h-100" src=${item.img} alt="Card image" style="height: 250px;object-fit: cover;">
             <div class="card-body">
